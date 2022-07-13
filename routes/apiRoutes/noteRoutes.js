7b2d2ef02fd127f61ca651371f9330dfcd8fdf33
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-13 14:34:27 -0400

    fixed bug causing page not to render

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,64 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../Develop/db/db.json')
-const { newNote, validNote, deleteNote } = require('../../lib/notes')
-const fs = require('fs')
+//const { newNote, validNote, deleteNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const fs = require('fs');
 const path = require('path');
+const generateUniqueId = require('generate-unique-id')
 
 router.get('/notes', (req, res) => {
-    fs.readFile(
-        path.join(__dirname, '../../db/db.json'), 'utf-8', (err, data) => {
-          let notes = JSON.parse(data);
-          if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-          } else {
-            res.json(notes);
-          }
-        }
-      );
+    let results = notes;
+    res.json(results)
 });
   
 
 router.post('/notes', (req, res) => {
 
-  req.body.id = uniqid();
+  const newNote = req.body
 
+  req.body.id = generateUniqueId();
 
-  fs.readFile(
-    path.join(__dirname, '../../db/db.json'), 'utf-8', (err, data) => {
-      let currentNotes = JSON.parse(data);
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      } else {
-  
-        if (!validNote(req.body)) {
-          res.status(400).send('The note is not properly formatted.');
-        } else {
+  notes.push(newNote);
 
-          let createNewNote = newNote(req.body, currentNotes);
-          res.json(createNewNote);
-        }
-      }
-    }
-  );
+  fs.writeFileSync(path.join(__dirname, '../../db/db.json'),JSON.stringify({ notes }, null, 2));
+
+  res.json(newNote);
+
 });
 
-router.delete('/notes/:id', (req, res) => {
+// router.delete('/notes/:id', (req, res) => {
 
-  fs.readFile(
-    path.join(__dirname, '../../db/db.json'), 'utf-8', (err, data) => {
-      let currentNotes = JSON.parse(data);
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      } else {
-        let updatedNotes = deleteNote(req.params, currentNotes);
-        res.json(updatedNotes);
-      }
-    }
-  );
-});
+//   fs.readFile(
+//     path.join(__dirname, '../../db/db.json'), 'utf-8', (err, data) => {
+//       let currentNotes = JSON.parse(data);
+//       if (err) {
+//         res.status(500).json({ error: err.message });
+//         return;
+//       } else {
+//         let updatedNotes = deleteNote(req.params, currentNotes);
+//         res.json(updatedNotes);
+//       }
+//     }
+//   );
+// });
 
 module.exports = router;
